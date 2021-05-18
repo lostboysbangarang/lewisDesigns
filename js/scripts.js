fb7a52919af4b4825d8a8a17cda4527cf37fbdb5
Hayden Lugo <haydenlugo96@gmail.com>
2021-05-17 21:56:21 -0400

    why wont this work

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+var tf=0;
+
+
+
+
+
+
+
+
+// window.onload = lessGo();
+
+// function lessGo() {
+//     FFS();
+// }
+
+// funtion FFS() {
+//     const itatchi= document.querySelectorAll(".animeL1");
+
+// }
+
+
+// funtion close(){
+//     if (tf==0) {
+//         document.getElementById("blicky")
+//     }
+//     console.log("fuck");
+// }
+
+// $(".blicky").onClick(function() {
+//     if (tf==0) {
+//         $(".animeL2").animate({left: "-=10vw"})
+//     }
+// })
+$(document).ready(function(){
+    document.getElementById("blicky").onclick=function() {
+        document.getElementById("blickyL2").style.animation=document.getElementById("blickyL2").dataset.anime+" "+document.getElementById("blickyL2").dataset.time+"ms forwards ease-in";
+        // document.getElementById("blickyL3").style.animation=document.getElementById("blickyL3").dataset.anime+" "+document.getElementById("blickyL3").dataset.time+"ms forwards ease-in";
+        console.log("fuick");
+    }
+});
