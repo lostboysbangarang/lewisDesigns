711acda612b34431fe54b522a0cd57c4f9b908da
Hayden Lugo <haydenlugo96@gmail.com>
2021-05-17 22:40:58 -0400

    repetitive clicks

--- js/scripts.js
+++ js/scripts.js
@@ -33,8 +33,17 @@ var tf=0;
 // })
 $(document).ready(function(){
     document.getElementById("blicky").onclick=function() {
-        document.getElementById("blickyL2").style.animation=document.getElementById("blickyL2").dataset.anime+" "+document.getElementById("blickyL2").dataset.time+"ms forwards ease-in";
-        document.getElementById("blickyL3").style.animation=document.getElementById("blickyL3").dataset.anime+" "+document.getElementById("blickyL3").dataset.time+"ms forwards ease-in";
+        if (tf==0){
+            document.getElementById("blickyL2").style.animation=document.getElementById("blickyL2").dataset.anime+" "+document.getElementById("blickyL2").dataset.time+"ms forwards ease-in";
+            document.getElementById("blickyL3").style.animation=document.getElementById("blickyL3").dataset.anime+" "+document.getElementById("blickyL3").dataset.time+"ms forwards ease-in";
+            tf=1;
+        } else {
+            tf=0;
+            document.getElementById("blickyL2").style.animation=document.getElementById("blickyL2").dataset.anime+"1 "+document.getElementById("blickyL2").dataset.time+"ms forwards ease-in";
+            document.getElementById("blickyL3").style.animation=document.getElementById("blickyL3").dataset.anime+"1 "+document.getElementById("blickyL3").dataset.time+"ms forwards ease-in";
+
+        }
+        
         console.log("fuick");
     }
 });
